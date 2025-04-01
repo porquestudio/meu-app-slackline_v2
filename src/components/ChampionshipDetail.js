b22c754b21c245badbcdfcc12587470aff6512c5
porquestudio <57155182+porquestudio@users.noreply.github.com>
2025-04-01 00:00:23 -0300

    ChampionshipDetail.js
    Detalhes do Campeonato (Exemplo de Tela para Gerenciar Atletas e Chaveamento)

---
+++ src/components/ChampionshipDetail.js
@@ -0,0 +1,17 @@
+// src/components/ChampionshipDetail.js
+import React from 'react';
+
+function ChampionshipDetail() {
+  // Nesta tela você implementará:
+  // • Cadastro de atletas com seleção de modalidade e nível (ex.: Trickline/Iniciante, etc.)
+  // • Opções para criar chaveamento automático ou manual
+  return (
+    <div>
+      <h2>Detalhes do Campeonato</h2>
+      <p>Aqui você pode cadastrar atletas, definir modalidades (Trickline ou Speedline) e níveis, além de criar o chaveamento.</p>
+      {/* Implemente os formulários e lógicas de acordo com as necessidades */}
+    </div>
+  );
+}
+
+export default ChampionshipDetail;
